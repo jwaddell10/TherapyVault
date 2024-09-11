693887e0ef409b0a8878056906545eb17093c217
jwaddell10 <115955410+jwaddell10@users.noreply.github.com>
2024-09-11 15:02:46 -0500

    created navbar, styled it minimally

--- PsychShelf_Frontend/src/components/HomePage/HomePage.jsx
+++ PsychShelf_Frontend/src/components/HomePage/HomePage.jsx
@@ -3,7 +3,7 @@ import Button from "../helpers/Button";
 export default function Home() {
 	return (
 		<div className="home-page-container">
-			<div className="title">PsychShelf</div>
+			<div className="title">TherapyVault</div>
             <div className="instructions">{`These are the instructions. I'm writing them here`}</div>
             <Button />
 		</div>
--- PsychShelf_Frontend/src/components/NavBar.jsx
+++ PsychShelf_Frontend/src/components/NavBar.jsx
@@ -1,8 +1,21 @@
 import { Outlet } from "react-router-dom";
+import { Link } from "react-router-dom";
+import "./NavBar.css";
 
 export default function NavBar() {
 	return (
 		<>
+			<nav className="navbar">
+				<ul>
+					<li className="title">
+						<Link to="/">TherapyVault</Link>
+					</li>
+					<li className="nav-link">
+						<Link to="/login">Log in</Link>
+						<Link to="/signup">Sign up</Link>
+					</li>
+				</ul>
+			</nav>
 			<Outlet />
 		</>
 	);
