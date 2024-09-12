41246b43d0b709a8da6d25bf7de96e7c7002a4fa
jwaddell10 <115955410+jwaddell10@users.noreply.github.com>
2024-09-12 15:28:05 -0500

    added local strategy to app.js

--- TherapyVault_Backend/app.js
+++ TherapyVault_Backend/app.js
@@ -1,50 +1,59 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var cors = require('cors');
-const session = require('express-session');
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-require('dotenv').config()
+var createError = require("http-errors");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+var cors = require("cors");
+
+const { Pool } = require("pg");
+const express = require("express");
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+const session = require("express-session");
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
+require("dotenv").config();
 
 var app = express();
 
 app.use(cors()); // Add this line to enable CORS
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use(session({
-  secret: 'keyboard cat',
-  resave: false,
-  saveUninitialized: false,
-  cookie: { secure: true }
-}));
+const pool = new Pool({
+	// add your configuration
+});
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use(session({ secret: "cats", resave: false, saveUninitialized: false }));
+app.use(passport.session());
+app.use(express.urlencoded({ extended: false }));
+
+app.get("/", (req, res) => res.render("index"));
+
+app.listen(3000, () => console.log("app listening on port 3000!"));
+
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.use(function (req, res, next) {
+	next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use(function (err, req, res, next) {
+	// set locals, only providing error in development
+	res.locals.message = err.message;
+	res.locals.error = req.app.get("env") === "development" ? err : {};
 
-  // send error as JSON instead of rendering a view
-  res.status(err.status || 500);
-  res.json({
-    message: err.message,
-    error: req.app.get('env') === 'development' ? err : {}
-  });
+	// send error as JSON instead of rendering a view
+	res.status(err.status || 500);
+	res.json({
+		message: err.message,
+		error: req.app.get("env") === "development" ? err : {},
+	});
 });
 
 module.exports = app;
