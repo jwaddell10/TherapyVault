d1ede4320547ff45f3cf46bc93f26a9bde790bcb
jwaddell10 <115955410+jwaddell10@users.noreply.github.com>
2024-09-29 17:24:40 -0500

    modified signup component so login styles would apply to signup component

--- client/src/components/Signup.jsx
+++ client/src/components/Signup.jsx
@@ -49,46 +49,37 @@ export default function Signup() {
 		<main className="signup">
 			<h1 className="title">Sign Up</h1>
 			<form method="POST" onSubmit={handleSubmit}>
-				<label htmlFor="">
-					Username:
-					<input
-						name="username"
-						type="text"
-						value={formData.username}
-						onChange={handleChange}
-						required
-					/>
-				</label>
+				<label htmlFor="">Username:</label>
+				<input
+					name="username"
+					type="text"
+					value={formData.username}
+					onChange={handleChange}
+					required
+				/>
 				{error && <div style={{ color: "red" }}>{error}</div>}
-
-				<label htmlFor="">
-					Password:
-					<input
-						name="password"
-						type="password"
-						value={formData.password}
-						onChange={handleChange}
-						required
-					/>
-				</label>
+				<label htmlFor="">Password:</label>{" "}
+				<input
+					name="password"
+					type="password"
+					value={formData.password}
+					onChange={handleChange}
+					required
+				/>
 				{passwordError && (
 					<div style={{ color: "red" }}>{passwordError}</div>
 				)}
-
-				<label htmlFor="">
-					Confirm Password:
-					<input
-						name="confirmPassword"
-						type="password"
-						value={formData.confirmPassword}
-						onChange={handleChange}
-						required
-					/>
-				</label>
+				<label htmlFor="">Confirm Password:</label>{" "}
+				<input
+					name="confirmPassword"
+					type="password"
+					value={formData.confirmPassword}
+					onChange={handleChange}
+					required
+				/>
 				{passwordError && (
 					<div style={{ color: "red" }}>{passwordError}</div>
 				)}
-
 				<Button type="submit" text="Sign up"></Button>
 			</form>
 		</main>
